be6f85e84e2b33f6aa6e1fc68c4eb0d41c8e5113
Anushka <anushkajalori@gmail.com>
2024-12-04 00:46:01 +0530

    i thought sorting each step would make it easier on the system, but input is pretty small

--- day1/solution_1.py
+++ day1/solution_1.py
@@ -5,9 +5,9 @@ l1=[]
 l2=[]
 for i in real_data:
     l1.append(int(i.split()[0]))
-    l1.sort()
     l2.append(int(i.split()[1]))
-    l2.sort()
+l1.sort()
+l2 .sort() 
 count = 0
 for i in range(len(l1)):
     count += l1[i] - l2[i] if l1[i] - l2[i] > 0 else (l1[i] - l2[i]) *-1
